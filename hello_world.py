a69a357daac83bcb1237c285abf43cc236d3e102
SeoYuChan0318 <dbcks0318@gmail.com>
2024-09-10 12:26:12 +0900

    Up_hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,9 @@
 def print_hello():
     print("Hello World!!")
+
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
 if __name__ == '__maine__':
+    print_KIT()
     print_hello()
